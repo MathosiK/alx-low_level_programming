855847233444418fa92525c5250ce42c3974a35f
MathosiK <Karabomathosi99@gmail.com>
2023-02-16 06:18:31 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+
+
 /**
- * main A program that prints a line with puts funcation
+ * main - A program that prints a line with puts funcation
  * Return: 0 (Success)
  */
 int main(void)
