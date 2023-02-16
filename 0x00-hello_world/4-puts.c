3169f5319bab84dcb8a3e2f4a90cb32480f6dc4e
MathosiK <Karabomathosi99@gmail.com>
2023-02-16 05:52:29 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main a program that prints a line with puts funcation
+ * Return : 0 (Success)
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);}
