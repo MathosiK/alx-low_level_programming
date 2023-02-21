1b15359199ca0298bd4dc49eaaf79452080b1016
MathosiK <Karabomathosi99@gmail.com>
2023-02-21 15:08:15 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifindef main_h
-#define main_h
+#ifndef main.h
+#define main.h
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,6 +13,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
 
 #endif
