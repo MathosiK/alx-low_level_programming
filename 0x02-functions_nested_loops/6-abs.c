17cba69090d0686330926d43571e326b41c97e9a
MathosiK <Karabomathosi99@gmail.com>
2023-02-22 01:09:04 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @c: number whose absolute value is to be found
+ *
+ * Return: Absolute value of the number 
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		a*= -1;
+	return (a);
+}
