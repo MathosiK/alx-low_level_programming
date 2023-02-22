68207b86cfebe8d725b50468652bb1d2db0926f2
MathosiK <Karabomathosi99@gmail.com>
2023-02-22 01:11:57 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,11 +3,11 @@
  *
  * @c: number whose absolute value is to be found
  *
- * Return: Absolute value of the number 
+ * Return: Absolute value of the number
  */
 int _abs(int a)
 {
 	if (a < 0)
-		a*= -1;
+		a *= -1;
 	return (a);
 }
