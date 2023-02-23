1c47fe7db84729720f86ca805ba3390cae33149c
MathosiK <Karabomathosi99@gmail.com>
2023-02-22 16:03:38 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Check Main
+ * @a: an integer a
+ * @b: an integer
+ * description: Function that adds two intergers and returns the result
+ * Return: Results to add a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
