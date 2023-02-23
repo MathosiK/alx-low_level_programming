91bf6b530d56011257f52ffe24957965793c2c30
MathosiK <Karabomathosi99@gmail.com>
2023-02-23 07:10:01 -0800

    1-main.c

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -12,5 +12,4 @@ int main(void)
         i = 98;
         positive_or_negative(i);
 
-        return (0);
-}
+        return (0);}
