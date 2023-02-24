4f369536369a30713313a13eb567bebf052e541f
MathosiK <Karabomathosi99@gmail.com>
2023-02-24 01:28:00 -0800

    0-isupper.c

--- 0-isupper.c
+++ 0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /*
  * _isupper - A function that checks for uppercase  character
- * @c: An input character
+ * Description: An input character
  * Return: 1 if c is uppercase or 0 otherwise
  */
 int _isupper(int c)
